6d2df95ef99b7ee5d15cbc46c4ee13883a8166fa
Magdalena <m.mandat@gmail.com>
2018-06-27 10:49:07 +0100

    adds a getter method for suittype in Card class

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CardTest {
 
@@ -9,7 +12,10 @@ public class CardTest {
         card = new Card(SuitType.HEARTS, RankType.KING);
     }
 
-
+    @Test
+    public void canGetSuit() {
+        assertEquals(SuitType.HEARTS, card.getSuit());
+    }
 
 
 }
