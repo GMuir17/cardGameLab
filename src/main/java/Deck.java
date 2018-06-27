04f72964e93ef2032bf1fd9ba7914e43c32e1951
Magdalena <m.mandat@gmail.com>
2018-06-27 10:59:21 +0100

    adds a cardCount method to Deck class

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    ArrayList<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<>();
+    }
+
+    public int cardCount() {
+        return this.cards.size();
+    }
 }
